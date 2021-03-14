c25b681441563fdd3ecdda00638088edeaf801c6
Corey Bailey <public@coreybailey.net>
2021-03-14 18:38:10 -0400

    Add examples

---
+++ src/example/index.js
@@ -0,0 +1,31 @@
+import Basic from './basic'
+import UrlParameters from './url-parameters'
+import Nesting from './nesting'
+import RedirectsAuth from './redirects-auth'
+import CustomLink from './custom-link'
+import PreventingTransitions from './preventing-transitions'
+import NoMatch404 from './no-match-404'
+import RecursivePaths from './recursive-paths'
+import Sidebar from './sidebar'
+import AnimatedTransitions from './animated-transitions'
+import RouteConfig from './route-config'
+import ModalGallery from './modal-gallery'
+import StaticRouterContext from './staticrouter-context'
+import QueryParameters from './query-parameters'
+
+export {
+    Basic,
+    UrlParameters,
+    Nesting,
+    RedirectsAuth,
+    CustomLink,
+    PreventingTransitions,
+    NoMatch404,
+    RecursivePaths,
+    Sidebar,
+    AnimatedTransitions,
+    RouteConfig,
+    ModalGallery,
+    StaticRouterContext,
+    QueryParameters,
+};
