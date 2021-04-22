111e844bfc69bdb79d2b8a996e6c2ac4411ba7a4
Corey Bailey <public@coreybailey.net>
2021-04-22 11:40:19 -0400

    Formatting

--- src/App.js
+++ src/App.js
@@ -26,22 +26,24 @@ function App() {
         <Link className='link' to='/react-router/url-parameter'>url-parameter</Link>
       </nav>
       <hr />
-      <Switch>
-        <Route exact path='/react-router/animate-transitions' component={cmpt.AnimatedTransitions} />
-        <Route exact path='/react-router/basic' component={cmpt.Basic} />
-        <Route exact path='/react-router/custom-link' component={cmpt.CustomLink} />
-        <Route exact path='/react-router/model-gallery' component={cmpt.ModalGallery} />
-        <Route exact path='/react-router/nesting' component={cmpt.Nesting} />
-        <Route exact path='/react-router/no-match-40' component={cmpt.NoMatch404} />
-        <Route exact path='/react-router/preventing-transitions' component={cmpt.PreventingTransitions} />
-        <Route exact path='/react-router/query-parameter' component={cmpt.QueryParameters} />
-        <Route exact path='/react-router/recursive-paths' component={cmpt.RecursivePaths} />
-        <Route exact path='/react-router/redirects-auth' component={cmpt.RedirectsAuth} />
-        <Route exact path='/react-router/route-config' component={cmpt.RouteConfig} />
-        <Route exact path='/react-router/sidebar' component={cmpt.Sidebar} />
-        <Route exact path='/react-router/static-router-context' component={cmpt.StaticRouterContext} />
-        <Route exact path='/react-router/url-parameter' component={cmpt.UrlParameters} />
-      </Switch>
+      <main>
+        <Switch>
+          <Route exact path='/react-router/animate-transitions' component={cmpt.AnimatedTransitions} />
+          <Route exact path='/react-router/basic' component={cmpt.Basic} />
+          <Route exact path='/react-router/custom-link' component={cmpt.CustomLink} />
+          <Route exact path='/react-router/model-gallery' component={cmpt.ModalGallery} />
+          <Route exact path='/react-router/nesting' component={cmpt.Nesting} />
+          <Route exact path='/react-router/no-match-40' component={cmpt.NoMatch404} />
+          <Route exact path='/react-router/preventing-transitions' component={cmpt.PreventingTransitions} />
+          <Route exact path='/react-router/query-parameter' component={cmpt.QueryParameters} />
+          <Route exact path='/react-router/recursive-paths' component={cmpt.RecursivePaths} />
+          <Route exact path='/react-router/redirects-auth' component={cmpt.RedirectsAuth} />
+          <Route exact path='/react-router/route-config' component={cmpt.RouteConfig} />
+          <Route exact path='/react-router/sidebar' component={cmpt.Sidebar} />
+          <Route exact path='/react-router/static-router-context' component={cmpt.StaticRouterContext} />
+          <Route exact path='/react-router/url-parameter' component={cmpt.UrlParameters} />
+        </Switch>
+      </main>
     </div>
   );
 }
